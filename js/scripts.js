819c0b86cf48c117d6cb8999ce766bbaedef4196
Kenny Wolfenberger and Tanner Damron <tanner@>
2018-10-16 11:49:52 -0700

    updated button alignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("button#dark").click(function() {
+    $("body").removeClass("lighttheme");
     $("body").addClass("darktheme");
   });
-
+  $("button#light").click(function() {
+    $("body").removeClass("darktheme");
+    $("body").addClass("lighttheme");
+  });
 });
